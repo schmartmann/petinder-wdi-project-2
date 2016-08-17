1142c09b451975466342130aec8c0ed973f18c73
Stefan Hartmann <stefanhartmann@gmail.com>
2016-08-16 23:05:18 -0400

    update api.js res.send() data obj

--- public/js/script.js
+++ public/js/script.js
@@ -14,6 +14,7 @@ window.onload = function(event){
         "success" : function(location_data){
           console.log("current location", location_data);
           location = location_data;
+          debugger
               $.ajax({
                 "url" : "/api/findPet/"+location,
                 "method" : "GET",
