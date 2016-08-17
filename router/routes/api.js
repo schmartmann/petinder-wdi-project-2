f3eb5430b9a25b6a0492c744cca1bbf4952d9966
Stefan Hartmann <stefanhartmann@gmail.com>
2016-08-17 09:41:12 -0400

    add log of google geo formatted_address

--- router/routes/api.js
+++ router/routes/api.js
@@ -10,6 +10,7 @@ router.get("/location/:latlng", function(req, res){
   request("https://maps.googleapis.com/maps/api/geocode/json?latlng="+latlng+"&key="+process.env.GOOGLE_GEOLOCATE_API, function(error, response, body){
     if (!error && response.statusCode == 200) {
       var data = JSON.parse(body);
+      console.log(data.results[0].formatted_address)
       var location = data.results[0].address_components[8].short_name;
       console.log("CURRENT ZIP", location);
       res.send({"zip":location})
